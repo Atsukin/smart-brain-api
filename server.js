6d2cc562f9676571f97bf50981db836db8ee1ce7
atsukin <atsuki.tamura.2740@gmail.com>
2020-06-21 16:00:45 +0900

    database change

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "postgresql-concave-71555",
-    user: "hamanishigaku",
-    password: "",
-    database: "smart-brain",
+    host: process.env.DATABASE_URL,
+    ssl: true,
   },
 });
 
